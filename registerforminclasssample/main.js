c024a251d92d0aa1098113453b4b1aae2cb1d446
Nithya Thayananthan <nithyathaya@gmail.com>
2021-11-23 11:35:20 -0500

    form validation

--- registerforminclasssample/main.js
+++ registerforminclasssample/main.js
@@ -1,1 +1,18 @@
+window.onload = function () {
+  document.getElementById('register').onclick = validate;
+};
 
+function validate(event) {
+  console.log(event);
+  event.preventDefault();
+  // alert('in function');
+  let fname = document.querySelector('#fname');
+  let namepattern = /^[a-zA-Z]{2,30}$/;
+  if (fname.value == '') {
+    fname.nextElementSibling.innerHTML = ' Please enter first name';
+  } else if (!namepattern.test(fname.value)) {
+    fname.nextElementSibling.innerHTML = 'Please enter name with letters only';
+  } else {
+    fname.nextElementSibling.innerHTML = '';
+  }
+}
--- registerforminclasssample/register/register.js
+++ registerforminclasssample/register/register.js
@@ -1,61 +1,61 @@
-"use strict";
+'use strict';
 
-const $ = selector => document.querySelector(selector); 
+const $ = (selector) => document.querySelector(selector);
 
 const processEntries = () => {
-    // get form controls to check for validity
-    const email = $("#email_address");
-    const phone = $("#phone");
-    const country = $("#country");
-    const terms = $("#terms");
+  // get form controls to check for validity
+  const email = $('#email_address');
+  const phone = $('#phone');
+  const country = $('#country');
+  const terms = $('#terms');
 
-    // check user entries for validity
-    let isValid = true;
-    if (email.value == "") {
-        email.nextElementSibling.textContent = "This field is required.";
-        isValid = false; 
-    } else {
-        email.nextElementSibling.textContent = ""; 
-    }
+  // check user entries for validity
+  let isValid = true;
+  if (email.value == '') {
+    email.nextElementSibling.textContent = 'This field is required.';
+    isValid = false;
+  } else {
+    email.nextElementSibling.textContent = '';
+  }
 
-    if (phone.value == "") {
-        phone.nextElementSibling.textContent = "This field is required.";
-        isValid = false; 
-    } else { 
-        phone.nextElementSibling.textContent = "";
-    }
+  if (phone.value == '') {
+    phone.nextElementSibling.textContent = 'This field is required.';
+    isValid = false;
+  } else {
+    phone.nextElementSibling.textContent = '';
+  }
 
-    if (country.value == "") {
-        country.nextElementSibling.textContent = "Please select a country.";
-        isValid = false; 
-    } else { 
-        $("#country").nextElementSibling.textContent = "";
-    }
+  if (country.value == '') {
+    country.nextElementSibling.textContent = 'Please select a country.';
+    isValid = false;
+  } else {
+    $('#country').nextElementSibling.textContent = '';
+  }
 
-    if (terms.checked == false) {
-        terms.nextElementSibling.textContent = "This box must be checked.";
-        isValid = false; 
-    } else { 
-        terms.nextElementSibling.textContent = "";
-    }
+  if (terms.checked == false) {
+    terms.nextElementSibling.textContent = 'This box must be checked.';
+    isValid = false;
+  } else {
+    terms.nextElementSibling.textContent = '';
+  }
 
-    // submit the form if all fields are valid
-    if (isValid == true) {
-        $("form").submit(); 
-    }
+  // submit the form if all fields are valid
+  if (isValid == true) {
+    $('form').submit();
+  }
 };
 
 const resetForm = () => {
-    $("form").reset();
-    $("#email_address").nextElementSibling.textContent = "*";
-    $("#phone").nextElementSibling.textContent = "*";
-    $("#country").nextElementSibling.textContent = "*";	
-    $("#terms").nextElementSibling.textContent = "*";
-    $("#email_address").focus();
+  $('form').reset();
+  $('#email_address').nextElementSibling.textContent = '*';
+  $('#phone').nextElementSibling.textContent = '*';
+  $('#country').nextElementSibling.textContent = '*';
+  $('#terms').nextElementSibling.textContent = '*';
+  $('#email_address').focus();
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    $("#register").addEventListener("click", processEntries);
-    $("#reset_form").addEventListener("click", resetForm);  
-    $("#email_address").focus();   
+document.addEventListener('DOMContentLoaded', () => {
+  $('#register').addEventListener('click', processEntries);
+  $('#reset_form').addEventListener('click', resetForm);
+  $('#email_address').focus();
 });
